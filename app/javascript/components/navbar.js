cc31103f4e7d40adc023b44b53fe540e85b8ee25
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-03 22:38:10 +0200

    changes

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,6 +2,9 @@ const navBar = () => {
   console.log('Bonjour');
   const navButton = document.querySelector(".mobile-button");
   let navMenu =  document.querySelector(".nav");
+  navButton.addEventListener("click", (event) => {
+    navMenu.classList.toggle("visible");
+  });
 
 }
 export { navBar };
