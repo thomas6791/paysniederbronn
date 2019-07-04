93f0758f3ced3d59a0c9ef21fb470501bd148f85
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-04 11:05:18 +0200

    change controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
     get 'about', to: 'pages#about', as: :about
     get 'contact', to: 'pages#contact', as: :contact
     get 'alentours', to: 'pages#alentours', as: :alentours
-    get 'holidayrentings', to: 'pages#holidayrentings', as: :holidayrentings do
-      collection do
-        get 'gittes-heidelbeeres'
-      end
-    end
+    #get 'holidayrentings', to: 'pages#holidayrentings', as: :holidayrentings do
+    #  collection do
+    #    get 'gittes-heidelbeeres'
+    #  end
+    #end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
