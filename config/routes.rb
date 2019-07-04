b8778c7c3bae9b47789fb24a57ffdd593aaaca9d
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-04 11:09:37 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'holiday_rentings/index'
+  get 'holiday_rentings/show'
+  get 'holiday_rentings/new'
+  get 'holiday_rentings/create'
+  get 'holiday_rentings/edit'
+  get 'holiday_rentings/update'
+  get 'holiday_rentings/destroy'
   scope '(:locale)', locale: /fr|en|de/ do
     root to: 'pages#home'
     get 'about', to: 'pages#about', as: :about
@@ -9,6 +16,11 @@ Rails.application.routes.draw do
     #    get 'gittes-heidelbeeres'
     #  end
     #end
+    resource :holiday_rentings do
+      collection do
+        get 'gites-heidelbeeres'
+      end
+    end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
