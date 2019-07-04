fd33e191b5a811a8c971ed44178e89db0378b956
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-04 11:20:14 +0200

    delete route

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,6 @@ Rails.application.routes.draw do
     get 'about', to: 'pages#about', as: :about
     get 'contact', to: 'pages#contact', as: :contact
     get 'alentours', to: 'pages#alentours', as: :alentours
-    #get 'holidayrentings', to: 'pages#holidayrentings', as: :holidayrentings do
-    #  collection do
-    #    get 'gittes-heidelbeeres'
-    #  end
-    #end
-    resource :holiday_rentings
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
