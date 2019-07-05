c40656d47157feb2231112945addfe39271c214b
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-05 16:16:25 +0200

    change

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
         get 'studio-heidelbeere', to: 'holiday_rentings#studio_heidelbeere', as: :studio_heidelbeere
       end
     end
-    get '/holiday_rentings/1', to: redirect('/meuble-heidelbeere')
     constraints(subdomain: 'gites-heidelbeere') do
       get '/', to: 'holiday_rentings#gites_heidelbeere', as: 'sub_heidelbeere'
     end
