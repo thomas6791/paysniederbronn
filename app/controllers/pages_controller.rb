6ffa7333c9f9906719ef9ac07ef8858150dca11d
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-07 20:55:03 +0200

    add route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def simulator
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get 'about', to: 'pages#about', as: :about
     get 'contact', to: 'pages#contact', as: :contact
     get 'alentours', to: 'pages#alentours', as: :alentours
+    get 'simulateur', to: 'pages#simulator', as: :simulateur
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :holiday_rentings do
       collection do                       # collection => no restaurant id in URL
