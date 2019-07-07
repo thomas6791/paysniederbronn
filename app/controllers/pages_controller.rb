58d18fc5d2a65aef2a81dc123daa66d9f6bf438b
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-07 22:43:44 +0200

    change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,9 +26,9 @@ class PagesController < ApplicationController
     days = params[:taxe_sejour][:days].to_f
     people = params[:taxe_sejour][:people].to_f
     minors = params[:taxe_sejour][:minors].to_f
-    people_pay = people - minors
+    @people_pay = people - minors
     @tax_day = ((amount / days / people) * 0.05 * 1.10).round(2,half: :up)
     @result = (@tax_day * people_pay * days).round(2)
-    redirect_to simulateur_path(tax_day: @tax_day, result: @result)
+    redirect_to simulateur_path(tax_day: @tax_day, result: @result, people_pay: @people_pay)
   end
 end
