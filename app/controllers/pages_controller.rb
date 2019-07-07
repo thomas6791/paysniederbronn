02f43a8e385ff0216a78acd453de91a2f66bc64a
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-07 23:15:37 +0200

    change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,6 +18,17 @@ class PagesController < ApplicationController
 
   def simulator
     @taxes = YAML.load(File.read("config/taxes.yml"))[:taxes]
+    if params[:results].present?
+      @tax_day = params[:results][0][:tax_day]
+      @result_nc = params[:results][0][:result]
+      @people_pay = params[:results][0][:people_pay].to_i
+      @days = params[:results][0][:days]
+      @one = @people_pay * @taxes[:one].to_f
+      @two = @people_pay * @taxes[:two].to_f
+      @three = @people_pay * @taxes[:three].to_f
+      @four = @people_pay * @taxes[:four].to_f
+      @five = @people_pay * @taxes[:five].to_f
+    end
   end
 
   def result
@@ -26,9 +37,9 @@ class PagesController < ApplicationController
     days = params[:taxe_sejour][:days].to_f
     people = params[:taxe_sejour][:people].to_f
     minors = params[:taxe_sejour][:minors].to_f
-    @people_pay = people - minors
-    @tax_day = ((amount / days / people) * 0.05 * 1.10).round(2,half: :up)
-    @result = (@tax_day * people_pay * days).round(2)
-    redirect_to simulateur_path(tax_day: @tax_day, result: @result, people_pay: @people_pay)
+    people_pay = people - minors
+    tax_day = ((amount / days / people) * 0.05 * 1.10).round(2,half: :up)
+    result = (tax_day * people_pay * days).round(2)
+    redirect_to simulateur_path(results: [tax_day: tax_day, result: result, people_pay: people_pay, days: days])
   end
 end
