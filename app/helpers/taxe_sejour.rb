de2ce200dd5007731e74515c177935916f663fa9
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-08 11:03:03 +0200

    change work

--- app/helpers/taxe_sejour.rb
+++ app/helpers/taxe_sejour.rb
@@ -1,14 +1,32 @@
 class TaxeSejour
-  attr_reader :name, :width, :length
-  def initialize(name, width, length)
-    @name = name
-    @width, @length = width, length
+  attr_reader :amount, :days, :people, :minors, :town
+  def initialize(amount, days, people, minors, town)
+    @amount = amount
+    @days = days
+    @people = people
+    @minors = minors
+    @town = town
+    @taxes = YAML.load(File.read("config/taxes.yml"))[:taxes][@town]
   end
-  def coucou
-    return "Bonsoir"
+  def people_pay
+    return @people - @minors
   end
 
-  def floor_area
-    @width * @length
+  def tax_day
+    ((@amount / @days / @people) * 0.05 * @taxes[:add_tax].to_f).round(2,half: :up)
+  end
+
+  def result
+    (self.tax_day * self.people_pay * @days).round(2)
+  end
+
+  def price_ratings
+    prices = []
+    prices << self.result
+    prices << (self.people_pay * @taxes[:one].to_f * @days).round(2)
+    prices << (self.people_pay * @taxes[:two].to_f * @days).round(2)
+    prices << (self.people_pay * @taxes[:three].to_f * @days).round(2)
+    prices << (self.people_pay * @taxes[:four].to_f * @days).round(2)
+    prices << (self.people_pay * @taxes[:five].to_f * @days).round(2)
   end
 end
