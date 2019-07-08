e7d63a52614ccd980b7a01f831385efec26f2933
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-08 09:21:35 +0200

    add class

---
+++ app/helpers/customer.rb
@@ -0,0 +1,14 @@
+class Customer
+  attr_reader :name, :width, :length
+  def initialize(name, width, length)
+    @name = name
+    @width, @length = width, length
+  end
+  def coucou
+    return "Bonsoir"
+  end
+
+  def floor_area
+    @width * @length
+  end
+end
