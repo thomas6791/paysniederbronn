3f305b7b2f958fda180bfb4b6013f7626d5b6663
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-13 19:39:49 +0200

    change

--- db/migrate/20190713173606_create_post_categories.rb
+++ db/migrate/20190713173606_create_post_categories.rb
@@ -1,8 +1,6 @@
 class CreatePostCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :post_categories do |t|
-      t.references :category, foreign_key: true
-      t.references :article, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_132302) do
+ActiveRecord::Schema.define(version: 2019_07_13_173606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "holiday_rentings", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
