d01e6588b79fc8996ced7158124387125f2b7f5f
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-15 22:56:30 +0200

    change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_category, only: [:show]
+  before_action :set_seo
   def index
     @articles = Article.all
   end
@@ -36,5 +37,11 @@ class ArticlesController < ApplicationController
   def set_category
     #@article = Article.find(params[:id])
     #@category = article.categories.first.name.parameterize
+  end
+
+  private
+
+  def set_seo
+    set_meta_tags noindex: true
   end
 end
--- app/controllers/subdomain_rentings_controller.rb
+++ app/controllers/subdomain_rentings_controller.rb
@@ -1,9 +1,16 @@
 class SubdomainRentingsController < ApplicationController
+  before_action :set_seo
   layout 'gite'
   def index
   end
 
   def simulator
     render 'pages/simulator'
+  end
+
+  private
+
+  def set_seo
+    set_meta_tags noindex: true
   end
 end
