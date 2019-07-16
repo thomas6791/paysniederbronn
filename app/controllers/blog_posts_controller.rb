e926240ac17763f76067a42273eda40cfe0721a8
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-16 20:53:40 +0200

    change

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,5 @@
 class BlogPostsController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -10,6 +11,22 @@ class BlogPostsController < ApplicationController
   end
 
   def create
+    @post = BlogPost.new(blog_params)
+    if @post.custom_date.blank?
+      @post.custom_date = @post.created_at
+    else
+      @post.custom_date = @post.custom_date
+    end
+
+    if @post.title.blank?
+      @post.title = @post.titre
+    else
+      @post.title = @post.title
+    end
+    author = AuthorBlog.find(params[:blog_page][:author_blog].to_i)
+    author.blog_pages << @post
+    @post.save
+    redirect_to blog_page_path(@post)
   end
 
   def edit
@@ -19,5 +36,14 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def blog_params
+    params.require(:blog_post).permit(:title, :description, :published, :titre, :content, :summary, :copyright, :author_blog_id, :custom_date, photos: [], blog_category_ids:[])
+  end
+
+  def set_page
+    @post = BlogPost.find_by!(slug: params[:id])
   end
 end
