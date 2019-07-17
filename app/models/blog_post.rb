b819f5ba85e9f698d9632694a336f595c7cd3607
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-17 10:02:22 +0200

    change

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,6 @@
 class BlogPost < ApplicationRecord
   belongs_to :author_blog
-  has_many :blog_post_categories
+  has_many :blog_post_categories, dependent: :destroy
   has_many :blog_categories, through: :blog_post_categories
   validates :title, uniqueness: { message: "cette title est déjà prise" }
   validates :titre, uniqueness: { message: "ce titre est déjà pris" }
