d1a5e4b00a392b00ef2576ea0bcfaaa5aaf5d778
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-07-20 11:10:04 +0200

    change db

--- db/migrate/20190720090351_change_article.rb
+++ db/migrate/20190720090351_change_article.rb
@@ -1,6 +1,5 @@
 class ChangeArticle < ActiveRecord::Migration[5.2]
   def change
-    add_column :articles, :title, :string
     add_column :articles, :slug, :string
     add_column :articles, :photos, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_075011) do
+ActiveRecord::Schema.define(version: 2019_07_20_090351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_07_17_075011) do
     t.string "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.string "photos"
   end
 
   create_table "author_blogs", force: :cascade do |t|
