d6444a2d27829a38ac68e5416574b537f450b24c
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-08-13 10:56:05 +0200

    add new controller + pages

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,15 @@
+class LandingPagesController < ApplicationController
+  def ligne_maginot_alsace
+  end
+  def chateaux_forts
+  end
+  def randonnees
+  end
+  def strasbourg
+  end
+  def alimentation
+  end
+  def cures_thermales
+  end
+
+end
