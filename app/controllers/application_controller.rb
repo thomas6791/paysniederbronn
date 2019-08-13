a307b0898f5f0ebaffcb3cafec5c08e57a66c0a1
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-08-13 10:45:10 +0200

    hreflang ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :set_url
 
   def set_locale
     I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
@@ -7,5 +8,8 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+  def set_url
+    @locale_url = request.fullpath
   end
 end
