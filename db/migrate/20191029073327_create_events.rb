4b6222db8c4ad46bb50b4092ec78d65bf16364b5
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-10-29 08:33:41 +0100

    create model

---
+++ db/migrate/20191029073327_create_events.rb
@@ -0,0 +1,10 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.datetime :start_time
+
+      t.timestamps
+    end
+  end
+end
