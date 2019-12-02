dd2ee26117c21cb0e9526bd6593c9be551660fa3
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-12-02 22:12:35 +0100

    change marche noel controller routes"

---
+++ app/controllers/marche_noels_controller.rb
@@ -0,0 +1,24 @@
+class MarcheNoelsController < ApplicationController
+
+  def index
+    set_meta_tags title: "Marchés de noel en Alsace",
+                description: "les marchés de noel dans l'ensemble de l'Alsace"
+    @marches = MarcheNoel.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @marche_noel = MarcheNoel.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
