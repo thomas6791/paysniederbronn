b10f68732461724bc985874ce7944f1e43052e6e
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-02-27 15:36:15 +0100

    change

--- app/javascript/components/taxes.js
+++ app/javascript/components/taxes.js
@@ -1,4 +1,6 @@
 const insertInput = () => {
   console.log("advisa");
+  const options = document.getElementById("options");
+  let button = document.querySelector(".fa-plus-square");
 }
 export { insertInput };
