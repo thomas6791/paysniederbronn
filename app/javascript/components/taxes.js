dce898717eb97ac7d52b7b53a371ef1978fc4d9d
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-02-27 16:10:13 +0100

    change

--- app/javascript/components/taxes.js
+++ app/javascript/components/taxes.js
@@ -2,10 +2,13 @@ const insertInput = () => {
   console.log("advisa");
   const options = document.getElementById("options");
   let icon = document.querySelector('.fa-plus-square');
+  let counter = 2
     icon.addEventListener("click", (event) => {
+      counter = counter + 1;
+      console.log(counter);
       options.insertAdjacentHTML("beforeend", `<div class="inline">
-          <input placeholder="Option 2, ex: petits déjeuner" class="option" type="text" name="taxe_sejour[option2]" id="taxe_sejour_option2">
-          <div class="input string optional taxe_sejour_tarif2"><input type="number" class="string optional" name="taxe_sejour[tarif2]" id="taxe_sejour_tarif2"></div>
+          <input placeholder="Option ${counter}" class="option" type="text" name="taxe_sejour[option${counter}]" id="taxe_sejour_option${counter}">
+          <div class="input string optional taxe_sejour_tarif${counter}"><input type="number" class="string optional" name="taxe_sejour[tarif${counter}]" id="taxe_sejour_tarif${counter}"></div>
         </div>`);
     });
 }
