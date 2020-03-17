c22bc7dbabd13ab6010fe32498340ce52ab571ab
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-03-17 21:44:14 +0100

    change

--- app/models/holiday_renting.rb
+++ app/models/holiday_renting.rb
@@ -3,6 +3,7 @@ class HolidayRenting < ApplicationRecord
   validates :titre, uniqueness: { message: "ce titre est déjà pris" }
   validates :slug, uniqueness: { message: "slug déjà prise" }
   after_validation :set_slug, only: [:create]
+  after_validation :set_category, only: [:create]
   has_rich_text :description
 
   def to_param
@@ -13,5 +14,9 @@ class HolidayRenting < ApplicationRecord
 
   def set_slug
     self.slug = titre.to_s.parameterize
+  end
+
+  def set_category
+    self.category = "renting"
   end
 end
