09c14fe39e0f3ac1e6b9532f779ef2bfeb3c05a9
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-03-20 19:54:34 +0100

    change breadcrumb

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -11,10 +11,7 @@ class BlogPostsController < ApplicationController
   end
 
   def show
-    if
-      @url[:action] != "index" || @url[:action] == "home"
-      add_breadcrumb "#{@url[:action].sub("_"," ").downcase}", "/blog/#{@url[:action].sub("_","-")}"
-    end
+    add_breadcrumb "#{@url[:action].sub("_"," ").downcase}", "/blog/#{@url[:action].sub("_","-")}" if ["home","index","show","edit","update"].exclude?(@url[:action])
     add_breadcrumb "#{@post.titre}", blog_post_path, title: "retour"
     @author = @post.author_blog
     set_meta_tags canonical: url_for(:only_path => false)
