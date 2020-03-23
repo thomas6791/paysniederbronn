d1b8500ada5ee4a4d505a34526f02f99720c4086
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-03-23 17:20:35 +0100

    change

--- app/controllers/holiday_rentings_controller.rb
+++ app/controllers/holiday_rentings_controller.rb
@@ -2,46 +2,29 @@ class HolidayRentingsController < ApplicationController
   before_action :set_seo
   before_action :set_data, only: [:show, :edit, :update, :destroy]
   def index
-    @annonces = HolidayRenting.all.where(category: "renting")
+    #@annonces = HolidayRenting.all.where(category: "renting")
   end
 
   def show
   end
 
   def new
-    @annonce  = HolidayRenting.new
+    #@annonce  = HolidayRenting.new
   end
 
   def create
-    @annonce  = HolidayRenting.new(renting_params)
-    @annonce.save
+    #@annonce  = HolidayRenting.new(renting_params)
+    #@annonce.save
   end
 
   def edit
   end
 
   def update
-    @annonce.update(renting_params)
+    #@annonce.update(renting_params)
   end
 
   def destroy
-  end
-
-  def gites_heidelbeere
-    require 'yaml'
-    if locale == :fr
-      @cards = YAML.load(File.read("config/cards.yml"))[:heidelbeere][locale.to_s]
-      render 'holiday_rentings/gites-heidelbeere/gites-heidelbeere'
-    elsif
-      @cards = YAML.load(File.read("config/cards.yml"))[:heidelbeere][locale.to_s]
-      render 'holiday_rentings/en.gites_heidelbeere'
-    else
-    end
-    @bonjour = "Hello"
-  end
-
-  def meuble_heidelbeere
-    render "holiday_rentings/gites-heidelbeere/#{locale.to_s}.meuble-heidelbeere"
   end
 
   private
