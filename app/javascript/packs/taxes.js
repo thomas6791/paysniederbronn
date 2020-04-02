242c36c343de5b7c67e0bca617a276222ac60f63
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-04-02 17:54:21 +0200

    add js remove element

--- app/javascript/packs/taxes.js
+++ app/javascript/packs/taxes.js
@@ -2,3 +2,5 @@ import { insertInput } from '../components/taxes';
 insertInput();
 import { test } from '../components/taxes';
 test();
+import { removeElement } from '../components/taxes';
+removeElement();
