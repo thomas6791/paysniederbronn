e272e2990ead0b89e441c85f3df95595ae6a2a4c
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-04-02 08:54:35 +0200

    change css and invoice form

--- app/helpers/taxe_sejour.rb
+++ app/helpers/taxe_sejour.rb
@@ -1,6 +1,6 @@
 class TaxeSejour
-  attr_reader :amount, :days, :people, :minors, :town, :price_ratings, :options
-  def initialize(amount, days, people, minors, town, options = 0)
+  attr_reader :amount, :days, :people, :minors, :town, :price_ratings, :options, :date, :client_name, :client_address, :invoice_number, :renting_name, :renting_address, :start_date, :end_date
+  def initialize(amount, days, people, minors, town, options = 0, date = "", client_name = "", client_adress = "", invoice_number = "", renting_name = "", renting_address = "", start_date = "", end_date = "")
     @amount = amount
     @days = days
     @people = people
@@ -9,6 +9,14 @@ class TaxeSejour
     @taxes = YAML.load(File.read("config/taxes.yml"))[:taxes][@town]
     @price_ratings = []
     @options = options
+    @date = date
+    @client_name = client_name
+    @client_address = client_address
+    @invoice_number = invoice_number
+    @renting_name = renting_name
+    @renting_address = renting_address
+    @start_date = start_date
+    @end_date = end_date
   end
   def people_pay
     return @people - @minors
