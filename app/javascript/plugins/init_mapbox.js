390fa41b89ebc4f3f9836b7eb12a266d9fa15c7b
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-05-26 22:40:04 +0200

    change

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,12 +3,6 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
-
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
@@ -17,11 +11,10 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
+    new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
     });
-    fitMapToMarkers(map, markers);
   }
 };
 
