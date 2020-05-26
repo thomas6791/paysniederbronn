9e43416be1807f537b519b33738238149419e528
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-05-26 19:56:20 +0200

    create scraping

--- app/helpers/rentings_helper.rb
+++ app/helpers/rentings_helper.rb
@@ -1,2 +1,37 @@
 module RentingsHelper
+  def scraping
+    require 'open-uri'
+    require 'nokogiri'
+
+    urls = ["https://www.niederbronn.com/se-loger/meubles-de-tourisme-chambres.html",
+      "https://www.niederbronn.com/se-loger/meubles-de-tourisme-chambres.html?a=1&calctot=1&limitstart=20",
+      "https://www.niederbronn.com/se-loger/meubles-de-tourisme-chambres.html?a=1&calctot=1&start=40",
+      "https://www.niederbronn.com/se-loger/meubles-de-tourisme-chambres.html?a=1&calctot=1&start=60",
+      "https://www.niederbronn.com/se-loger/meubles-de-tourisme-chambres.html?a=1&calctot=1&start=80"
+    ]
+
+    loc_urls = []
+
+
+    urls.each do |url|
+
+      html_file = open(url).read
+      html_doc = Nokogiri::HTML(html_file)
+
+      html_doc.search('.joomlei_result a').each do |element|
+        #puts element.attribute('href').value
+        loc_urls << "https://www.niederbronn.com#{element.attribute('href').value}"
+      end
+
+    end
+
+    loc_urls.each do |url|
+      html_file = open(url).read
+      html_doc = Nokogiri::HTML(html_file)
+
+    end
+
+    puts loc_urls
+    puts loc_urls.size
+  end
 end
