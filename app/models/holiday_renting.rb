7c6eff575b6acda4c0fd972504788ce68d2de8c1
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-05-27 21:49:12 +0200

    change full address

--- app/models/holiday_renting.rb
+++ app/models/holiday_renting.rb
@@ -6,7 +6,7 @@ class HolidayRenting < ApplicationRecord
   has_rich_text :description
   has_many_attached :photos
 
-  geocoded_by :address
+  geocoded_by :full_address
   after_validation :geocode, if: :will_save_change_to_address?
 
   def to_param
@@ -17,5 +17,9 @@ class HolidayRenting < ApplicationRecord
 
   def set_slug
     self.slug = titre.to_s.parameterize
+  end
+
+  def full_address
+    [address, zip_code, city].compact.join(', ')
   end
 end
