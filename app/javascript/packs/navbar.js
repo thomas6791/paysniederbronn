6f3528c824886c98ea41a371ec8e8f6883efce4c
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-05-27 21:03:58 +0200

    change

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,2 +1,18 @@
 import { navBar } from '../components/navbar';
 navBar();
+
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = "pk.eyJ1IjoidGhvbWFzLTY3IiwiYSI6ImNrYW85NDVtejFpY2oycm82Zm15cmY5b2YifQ.wRce3zl0dvvYMd0sMPL-2w";
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+  }
+};
+initMapbox();
+
