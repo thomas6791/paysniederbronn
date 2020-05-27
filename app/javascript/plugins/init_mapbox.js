0e25914af865442550aefcc838e77c14e26dfef0
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-05-27 23:02:42 +0200

    change map style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/thomas-67/ckaptvby03bg61iqr2jvmlcv5'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
