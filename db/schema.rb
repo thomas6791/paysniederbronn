69b66d318e2e181a8845c6abc93340973af8f94f
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-07-04 15:35:43 +0200

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -147,7 +147,6 @@ ActiveRecord::Schema.define(version: 2020_07_04_132018) do
   create_table "rentings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "thomas"
     t.string "titre"
     t.text "description"
     t.string "summary"
