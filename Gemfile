78919063ebee2f42c4fed5e26a839f119fb62684
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-06 20:32:52 +0100

    config

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ gem 'devise'
 
 gem 'turbolinks_render'
 
+gem 'letter_opener', group: :development
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/mailers/renting_mailer.rb
+++ app/mailers/renting_mailer.rb
@@ -6,8 +6,8 @@ class RentingMailer < ApplicationMailer
   #   en.renting_mailer.rentingask.subject
   #
   def rentingask
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = params[:user] # Instance variable => available in view
+    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    # This will render a view in `app/views/user_mailer`!
   end
 end
