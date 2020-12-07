31a9c4bb116e19101c71c0ba91bd08f436374819
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-07 19:51:47 +0100

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
+  config.action_mailer.default_url_options = { host: "paysniederbronn.fr" }
 end
