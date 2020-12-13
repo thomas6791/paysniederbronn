cd27655b453655f5b27b91689d5ef88803a6941a
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-13 21:29:51 +0100

    change

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,15 +3,24 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "content" ]
 
+
   connect() {
-    console.log(this.contentTarget);
-    console.log("Alien");
+    document.querySelectorAll('input[data-form-target="content"]').forEach((input) =>{
+      input.addEventListener("input", (event) => {
+        if (event.currentTarget.value !== undefined || event.currentTarget.value !== "") {
+          event.currentTarget.classList.remove("error");
+        }
+        else {}
+      });
+    });
   }
+
   submitForm() {
     let inputs = document.querySelectorAll('input[data-form-target="content"]');
     inputs.forEach((input) => {
       if (input.value === "") {
         input.classList.add("error");
+        input.placeholder = "Ce champ est requis";
         event.preventDefault();
       }
       else {}
