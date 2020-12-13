720ab745c328c9a876ec6ee6d272e277c6f67335
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-13 15:53:43 +0100

    change

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ['content']
+
+  connect() {
+    console.log(this.contentTarget)
+  }
+  sayHello() {
+    this.contentTarget.innerHTML = "HELLO !"
+  }
+}
