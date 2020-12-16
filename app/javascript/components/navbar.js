63c7e88c91948f13ebdad94d1b44b0247a28c7dc
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-16 21:40:59 +0100

    change

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -8,10 +8,10 @@ const navBar = () => {
 
 }
 const cookieFullscreen = () => {
-  const fullScreen = document.querySelector(".cookies-eu.fullscreen");
-  const coookieButton = document.querySelector(".cookies-eu-ok.js-cookies-eu-ok");
-  coookieButton.addEventListener("click", (event) =>{
-    fullscreen.style.display = "none";
+  let screen = document.querySelector(".cookies-eu.fullscreen");
+  let coookieButton = document.querySelector(".cookies-eu-ok.js-cookies-eu-ok");
+  coookieButton.addEventListener( "click", (event) => {
+    screen.style.display = "none";
   });
 }
 export { navBar };
