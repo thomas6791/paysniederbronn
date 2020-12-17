97d8efe3d496287ec6f9e8a67e926b046de4dafb
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-17 19:44:31 +0100

    chanfe

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -10,11 +10,14 @@ const navBar = () => {
 const cookieFullscreen = () => {
   const head = document.querySelector('head');
   let screen = document.querySelector(".cookies-eu.fullscreen");
-  let coookieButton = document.querySelector(".cookies-eu-ok.js-cookies-eu-ok");
-  coookieButton.addEventListener( "click", (event) => {
+  if (document.querySelector(".cookies-eu-ok.js-cookies-eu-ok") !== null) {
+    let coookieButton = document.querySelector(".cookies-eu-ok.js-cookies-eu-ok");
+    coookieButton.addEventListener( "click", (event) => {
     screen.style.display = "none";
     head.insertAdjacentHTML("afterbegin", `
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146085492-1"></script>
+      <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+      <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-HGY5YE4XT5&amp;l=dataLayer&amp;cx=c"></script>
+      <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-146085492-1"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
@@ -23,7 +26,8 @@ const cookieFullscreen = () => {
         gtag('config', 'UA-146085492-1');
       </script>
       `);
-  });
+    });
+  }
 }
 export { navBar };
 export { cookieFullscreen };
