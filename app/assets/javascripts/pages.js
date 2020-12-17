a31bf1acd64120949269d406953ca498440540db
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-17 19:17:33 +0100

    change

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+const cookieOK = () => {
+  const head = document.querySelector('head');
+  head.insertAdjacentHTML("afterbegin", `
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146085492-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-146085492-1');
+    </script>
+    `);
+}
--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -8,10 +8,21 @@ const navBar = () => {
 
 }
 const cookieFullscreen = () => {
+  const head = document.querySelector('head');
   let screen = document.querySelector(".cookies-eu.fullscreen");
   let coookieButton = document.querySelector(".cookies-eu-ok.js-cookies-eu-ok");
   coookieButton.addEventListener( "click", (event) => {
     screen.style.display = "none";
+    head.insertAdjacentHTML("afterbegin", `
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146085492-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-146085492-1');
+      </script>
+      `);
   });
 }
 export { navBar };
