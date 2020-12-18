0312ce42b821a5b97711e162a7f5bf93e733e7d6
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-12-19 00:23:35 +0100

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   before_action :set_locale
-  before_action :open_graph
 
   private
   def set_locale
@@ -14,12 +13,4 @@ class ApplicationController < ActionController::Base
      { locale: I18n.locale }
   end
 
-  def open_graph
-    set_meta_tags og: {
-      title:    'Pays de Niederbronn-les-Bains',
-      type:     'website',
-      url:      'https://wwww.paysniederbronn.fr',
-      image:    'https://www.paysniederbronn.fr/fr/assets/images/opengraph.png'
-    }
-  end
 end
