71629a339183c6e28702174e97bc8eb979cd95c9
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-02-18 23:20:24 +0100

    change

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -46,6 +46,7 @@ class RentingsController < ApplicationController
   def edit
     set_meta_tags noindex: true
     @annonce = Renting.find(params[:id])
+    @annonce.frequent_asks.build
   end
 
   def update
@@ -185,7 +186,7 @@ class RentingsController < ApplicationController
   private
 
   def renting_params
-    params.require(:renting).permit(:titre, :description, :summary, :avatar, :capacity, :city, :zip_code, :address, :latitude, :longitude, :website, :email, :tel, :category, :airbnb, :booking, :price_day, :price_week, :price_cure, photos: [], renting_category_ids: [], frequent_asks_attributes: [:question, :answer, :public])
+    params.require(:renting).permit(:titre, :description, :summary, :avatar, :capacity, :city, :zip_code, :address, :latitude, :longitude, :website, :email, :tel, :category, :airbnb, :booking, :price_day, :price_week, :price_cure, photos: [], renting_category_ids: [], frequent_asks_attributes: [:id, :question, :answer, :public])
   end
 
   def set_seo
