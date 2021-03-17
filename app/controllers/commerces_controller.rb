524ad248e831e893b7b06f26d20a96edf0a75ca2
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-03-17 21:28:31 +0100

    change

--- app/controllers/commerces_controller.rb
+++ app/controllers/commerces_controller.rb
@@ -1,14 +1,20 @@
 class CommercesController < ApplicationController
   def index
+    @commerces = Commerce.all
   end
 
   def show
   end
 
   def new
+    @commerce = Commerce.new
   end
 
   def create
+    @commerce = Commerce.new(commerce_params)
+    @commerce.save
+
+    redirect_to commerce_path(@commerce)
   end
 
   def edit
@@ -18,5 +24,9 @@ class CommercesController < ApplicationController
   end
 
   def destroy
+  end
+
+  def commerce_params
+    params.require(:commerce).permit(:name, :address)
   end
 end
