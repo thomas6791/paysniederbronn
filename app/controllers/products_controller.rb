1de5e1b0f61c6551f9f85ba584197b584f9f9f83
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-03-22 22:38:05 +0100

    change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name)
+    params.require(:product).permit(:name, :photo)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :commerce
   has_many :cart_items
+  has_one_attached :photo
 end
