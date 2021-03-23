ccbf5c1e85f93899663c873cce6323ee2b356f33
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-03-23 21:24:59 +0100

    change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   def create
+    @cart = session[:cart][params[:commerce_id]]
+    fail
     #teddy = Teddy.find(params[:teddy_id])
     #order  = Order.create!(teddy: teddy, teddy_sku: teddy.sku, amount: teddy.price, state: 'pending', user: current_user)
 
