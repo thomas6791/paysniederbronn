51789b23d4cc3bfb7ec5c0e2341834dc1528f1a4
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-03-24 20:18:01 +0100

    change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,22 @@ class OrdersController < ApplicationController
   def create
     @cart = session[:cart][params[:commerce_id]]
     commerce =  Commerce.find(params[:commerce_id])
-    fail
     #teddy = Teddy.find(params[:teddy_id])
     #order  = Order.create!(teddy: teddy, teddy_sku: teddy.sku, amount: teddy.price, state: 'pending', user: current_user)
+    @order = Order.new
+    order_arr = []
+    @cart.each do |item|
+      order_arr << item if item["sub_total"]["cents"] != 0
+    end
+    amount = 0
+    order_arr.each do |item|
+      amount += item["sub_total"]["cents"]
+    end
+    @order.amount_cents = amount
+    @order.delivery_date = params[:order][:delivery_date]
 
+    fail
+    @order.save
 
     #redirect_to new_order_payment_path(order)
   end
