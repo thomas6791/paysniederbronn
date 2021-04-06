9ee73d5c0cbe98ba09d9b812d6ea461b9f4217c5
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-04-06 13:03:08 +0200

    change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,6 @@ console.log('Hello World from Webpacker');
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-const application = Application.start()
-const context = require.context("../controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
