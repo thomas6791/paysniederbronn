9d365f7cd5735ff665a70be819ba2d7575c72c44
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-04-11 23:28:20 +0200

    change

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Paysniederbronn
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.time_zone = 'Paris'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
