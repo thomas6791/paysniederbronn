5bdfef0e27e7ca43d4a3061c60cc259d74c4c931
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-07-06 19:45:00 +0200

    change

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "tabs", "outpout" ]
+  static targets = [ "tabs", "tab", "outpout", "tab-contents", "tab-content" ]
   connect() {
     console.log("Parasite");
   }
-  select() {}
+  select() {
+    debugger;
+  }
 }
