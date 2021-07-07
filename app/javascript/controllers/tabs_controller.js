298f5c3b78696f1c8f0b1c71641ed6d9befbb0ce
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-07-07 13:14:56 +0200

    change

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -11,10 +11,10 @@ export default class extends Controller {
     let index = event.currentTarget.dataset.index;
     index = parseInt(index);
     Array.from(this.tabcontentsTarget.children).forEach( (element) =>{
-      element.style.display = 'none';
-      element.style.zIndex = 0;
+      element.style.position = 'absolute';
+      element.style.zIndex = -1;
     });
-    this.tabcontentsTarget.children[index].style.display = '';
+    this.tabcontentsTarget.children[index].style.position = 'inherit';
     this.tabcontentsTarget.children[index].style.zIndex = 1;
   }
 }
